2e52be9570e2c181d102fafcc0828ab97f143ccf
Iñaki Úcar <i.ucar86@gmail.com>
2017-02-21 21:09:29 +0100

    parallel run

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-FILE="results.dat"
 MANAGERS="Parf Aparf Rrpaa Prcs MinstrelBlues"
+REP=3
+
+job() {
+  local OUT=$(./waf --run "power-adaptation-distance --manager=ns3::${1}WifiManager")
+  echo "$OUT $1"
+}
+export -f job
 
 for i in $MANAGERS; do
-  for j in $(seq 1 10); do
-    OUT=$(./waf --run "power-adaptation-distance --manager=ns3::${i}WifiManager")
-    echo "$OUT $i" >> $FILE
+  for j in $(seq 1 $REP); do
+    sem -j+0 job $i 2>/dev/null
   done
 done
+sem --wait
