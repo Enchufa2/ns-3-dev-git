989ce50ebb4ed8d3f785a280389430646dd5f282
Iñaki Úcar <i.ucar86@gmail.com>
2017-02-21 20:40:00 +0100

    several repetitions

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-OUT="results.dat"
+FILE="results.dat"
 MANAGERS="Parf Aparf Rrpaa Prcs MinstrelBlues"
 
 for i in $MANAGERS; do
   for j in $(seq 1 10); do
-    ./waf --run "power-adaptation-distance --manager=ns3::${i}WifiManager" >> $OUT
+    OUT=$(./waf --run "power-adaptation-distance --manager=ns3::${i}WifiManager")
+    echo "$OUT $i" >> $FILE
   done
 done
