8a2ca6421ccdc4422c9d46b5124510a49b6c5dc4
Carlos Donato <carlosdonato90@gmail.com>
2017-02-22 12:02:14 +0100

    Added random seed to run script

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ MANAGERS="Parf Aparf Rrpaa Prcs MinstrelBlues"
 REP=3
 
 job() {
-  local OUT=$(./waf --run "power-adaptation-distance --manager=ns3::${1}WifiManager")
+  local OUT=$(NS_GLOBAL_VALUE="RngRun=$2" ./waf --run "power-adaptation-distance --manager=ns3::${1}WifiManager")
   echo "$OUT $1"
 }
 export -f job
 
 for i in $MANAGERS; do
   for j in $(seq 1 $REP); do
-    sem -j+0 job $i 2>/dev/null
+    sem -j+0 job $i $j 2>/dev/null
   done
 done
 sem --wait 2>/dev/null
